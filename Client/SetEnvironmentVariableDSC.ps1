cda71379f0546480aea0511a9ec9351e6f60c76a
kenjioshio <kenji_oshio@ot.olympus.co.jp>
2017-10-31 10:12:20 +0900

    Add a config for another environment variable

--- Client/SetEnvironmentVariableDSC.ps1
+++ Client/SetEnvironmentVariableDSC.ps1
@@ -1,15 +1,20 @@
 ﻿Configuration SetEnvironmentVariable
 {
-    Import-DscResource -ModuleName PSDesiredStateConfiguration
-    
+    Import-DscResource -ModuleName PSDesiredStateConfiguration    
     Node localhost
     {
-        Environment EnvironmentVariableSettings
-        {
+        Environment EnvironmentVariable_Path{
             Ensure = "Present"  # You can also set Ensure to "Absent"
             Name = "Path"
             Value = "C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\ProgramData\chocolatey\bin;C:\Program Files\TortoiseSVN\bin;C:\Program Files (x86)\NUnit 2.6.4\bin;C:\Program Files\Git\cmd"
         }
+
+        Environment EnvironmentVariable_VS120COMNTOOLS{
+            Ensure = "Present"  # You can also set Ensure to "Absent"
+            Name = "VS120COMNTOOLS"          
+            Value = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\"
+        }
+
     }
 }
 
