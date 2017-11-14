f8a5427d06b5d169e42759c0a149eacd99218e88
kenjioshio <kenji_oshio@ot.olympus.co.jp>
2017-11-14 18:14:21 +0900

    Add a firewall DSC and modify UAC DSC

---
+++ Client/AddFirewallRule.ps1
@@ -0,0 +1,39 @@
+﻿# Version="1.0.0.0"
+# Product="" 　　　
+# Copyright="Kenji Oshio" 
+# Company=""
+# DSC configuration for Firewall 
+#  
+
+# 本Configurationは64bitのResourceを使用するので、Program Files(x86)配下のPowershellから実行する際には注意すること。
+# xNetworkingが内部で読んでいるMSFT_xXXXXは64bitで実行されるのでProgram Files配下のPowershellのExecution PolicyがRemote Signed以上にセットしないと動かないでーす。
+# (Start-DscConfiguration実行時にこけます。)
+
+configuration Add_FirewallRuleToExistingGroup 
+{ 
+    Import-DscResource -ModuleName PSDesiredStateConfiguration 
+    Import-DscResource -ModuleName xNetworking
+
+    Node localhost
+    { 
+        xFirewall Firewall_UDP 
+        { 
+            Name                  = 'UDP 123' 
+            DisplayName           = 'UDP 123' 
+            Enabled               = 'True' 
+            Ensure                = 'Present' 
+            Direction             = 'Inbound'
+            Action                ='Allow'
+            Protocol              = 'UDP'
+            LocalPort             = ('123')
+            Profile               = ('Domain', 'Private','Public')
+            Description           = 'This is a test configuration'
+#            Program               = 'c:\windows\system32\notepad.exe'
+#            Service               = 'WinRM'
+
+        } 
+    } 
+ }
+
+Add_FirewallRuleToExistingGroup -OutputPath .\Add_FirewallRuleToExistingGroup 
+Start-DscConfiguration -Wait -Force -Verbose -Path .\Add_FirewallRuleToExistingGroup  -ComputerName Localhost
--- Client/SetUAC.ps1
+++ Client/SetUAC.ps1
@@ -11,19 +11,19 @@ Configuration UAC
     Node localhost
     {
         #該当マシンのUACを変更します。
-           xUAC localPCUACConifgration
-        {
-            #* UAC are enable for following values of Registory key. .The registory key = 1 in these cases.
-            #Setting ="AlwaysNotify"
-            #Setting ="NotifyChanges"
-            #Setting ="NotifyChangesWithoutDimming"
-            #Setting ="NeverNotify"
+       xUAC localPCUACConifgration
+       {
+            #* UAC has 4 levels as below;
+            #Setting ="AlwaysNotify" #Level3: "In Windows GUI, the slider is shown most higest position. "
+            #Setting ="NotifyChanges" #Level2: "This is Windwos default vaule. "
+            #Setting ="NotifyChangesWithoutDimming" #Level1
+            #Setting ="NeverNotify" #Level0: "In Windows GUI, the slider is shown most lowest position. And this configuration is not recommended." 
+            #                              : "It should be changed to the other more higer level usually."
 
-            #* UAC is disable.The registory key = 0 in this case.
             Setting = "NeverNotifyAndDisableAll"
-        }
-    
+       }
     }
 }
+
 UAC　-OutputPath .\UAC
 Start-DscConfiguration -Wait -Force -Verbose -Path .\UAC -ComputerName Localhost
