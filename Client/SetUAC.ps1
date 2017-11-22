3077dc6ec2281dc7e4c2195b8407c1ff27f7c898
kenjioshio <kenji_oshio@ot.olympus.co.jp>
2017-11-22 13:48:09 +0900

    Modify comment for each UAC level.

--- Client/SetUAC.ps1
+++ Client/SetUAC.ps1
@@ -13,14 +13,16 @@ Configuration UAC
         #該当マシンのUACを変更します。
        xUAC localPCUACConifgration
        {
-            #* UAC has 4 levels as below;
-            #Setting ="AlwaysNotify" #Level3: "In Windows GUI, the slider is shown most higest position. "
-            #Setting ="NotifyChanges" #Level2: "This is Windwos default vaule. "
-            #Setting ="NotifyChangesWithoutDimming" #Level1
-            #Setting ="NeverNotify" #Level0: "In Windows GUI, the slider is shown most lowest position. And this configuration is not recommended." 
-            #                              : "It should be changed to the other more higer level usually."
+            #[Note]UAC has 5 levels as below(However Windows GUI shows only 4 level. Please keep it in your mind.);
+            #Setting ="AlwaysNotify" # Level3: "In Windows GUI, the slider is shown most higest position. "
+            #Setting ="NotifyChanges" # Level2: "This is Windwos default vaule. "
+            #Setting ="NotifyChangesWithoutDimming" # Level1
+            #Setting ="NeverNotify" # Level0: "Once UAC is off, people that log on as administrator will always have the permissions of an administrator." 
+                                    #       : "It should be changed to the other more higer level usually."
+                                    #       : "The value of 'EnableLUA' is '1' when the UAC setting is equal to or higher than this level."
 
-            Setting = "NeverNotifyAndDisableAll"
+            Setting = "NeverNotifyAndDisableAll" # Level0'?: "In Windows GUI, the slider is shown most lowest position. The slider position is same as that of 'NeverNotify'. "
+                                                 #           "And this configuration is not recommended."The value of 'EnableLUA' is '0'." 
        }
     }
 }
